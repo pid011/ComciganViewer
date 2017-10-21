ef5f8371a800d1ccaa6a8cfe2a48bf2cd2d2d096
pid011 <deet1022@gmail.com>
2017-10-22 08:51:11 +0900

    프로젝트 갈아엎기

---
+++ ComciganViewer/ComciganViewer/Activities/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Webkit;
+
+namespace ComciganViewer
+{
+    [Activity(Label = "시간표", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            WebView web_view = FindViewById<WebView>(Resource.Id.main_webview);
+            web_view.SetWebViewClient(new ComciganWebViewClient());
+            web_view.LoadUrl("http://112.186.146.96:4080/");
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetScrollContainer(true);
+            web_view.Settings.DomStorageEnabled = true;
+        }
+    }
+}
+
---
+++ ComciganViewer/ComciganViewer/ComciganWebViewClient.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Webkit;
+
+namespace ComciganViewer
+{
+	public class ComciganWebViewClient : WebViewClient
+	{
+		[Obsolete]
+		public override bool ShouldOverrideUrlLoading(WebView view, string url)
+		{
+			view.LoadUrl(url);
+			return true;
+		}
+	}
+}
