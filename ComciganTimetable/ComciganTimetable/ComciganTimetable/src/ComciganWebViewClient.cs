263830c991ae1b71e7c9ac6c77be87ad15a1724e
pid011 <deet1022@gmail.com>
2017-10-21 11:45:25 +0900

    Up to supported Android Version & Add Obsolete attribute

--- ComciganTimetable/ComciganTimetable/ComciganTimetable/src/ComciganWebViewClient.cs
+++ ComciganTimetable/ComciganTimetable/ComciganTimetable/src/ComciganWebViewClient.cs
@@ -15,6 +15,7 @@ namespace Timetable
 {
 	public class ComciganWebViewClient : WebViewClient
 	{
+		[Obsolete]
 		public override bool ShouldOverrideUrlLoading(WebView view, string url)
 		{
 			view.LoadUrl(url);
