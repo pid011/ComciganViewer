eaa8c8925c46a0a66fe3c05a5cdd182f31b13610
pid011 <deet1022@gmail.com>
2017-10-22 15:25:01 +0900

    Remove menu icon

--- ComciganViewer/ComciganViewer/Resources/Resource.Designer.cs
+++ ComciganViewer/ComciganViewer/Resources/Resource.Designer.cs
@@ -2239,26 +2239,26 @@ namespace ComciganViewer
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
+			// aapt resource value: 0x7f02006a
+			public const int avd_hide_password_1 = 2130837610;
+			
 			// aapt resource value: 0x7f02006b
-			public const int avd_hide_password_1 = 2130837611;
+			public const int avd_hide_password_2 = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int avd_hide_password_2 = 2130837612;
-			
-			// aapt resource value: 0x7f02006d
-			public const int avd_hide_password_3 = 2130837613;
+			public const int avd_hide_password_3 = 2130837612;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
+			// aapt resource value: 0x7f02006d
+			public const int avd_show_password_1 = 2130837613;
+			
 			// aapt resource value: 0x7f02006e
-			public const int avd_show_password_1 = 2130837614;
+			public const int avd_show_password_2 = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int avd_show_password_2 = 2130837615;
-			
-			// aapt resource value: 0x7f020070
-			public const int avd_show_password_3 = 2130837616;
+			public const int avd_show_password_3 = 2130837615;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2279,52 +2279,49 @@ namespace ComciganViewer
 			public const int design_snackbar_background = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int ic_menu_blue_50_48dp = 2130837595;
+			public const int navigation_empty_icon = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int navigation_empty_icon = 2130837596;
+			public const int notification_action_background = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int notification_action_background = 2130837597;
+			public const int notification_bg = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_bg = 2130837598;
+			public const int notification_bg_low = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg_low = 2130837599;
+			public const int notification_bg_low_normal = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low_normal = 2130837600;
+			public const int notification_bg_low_pressed = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_low_pressed = 2130837601;
+			public const int notification_bg_normal = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_normal = 2130837602;
+			public const int notification_bg_normal_pressed = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_bg_normal_pressed = 2130837603;
-			
-			// aapt resource value: 0x7f020064
-			public const int notification_icon_background = 2130837604;
-			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_bg = 2130837609;
-			
-			// aapt resource value: 0x7f02006a
-			public const int notification_template_icon_low_bg = 2130837610;
-			
-			// aapt resource value: 0x7f020065
-			public const int notification_tile_bg = 2130837605;
-			
-			// aapt resource value: 0x7f020066
-			public const int notify_panel_notification_icon_bg = 2130837606;
-			
-			// aapt resource value: 0x7f020067
-			public const int tooltip_frame_dark = 2130837607;
+			public const int notification_icon_background = 2130837603;
 			
 			// aapt resource value: 0x7f020068
-			public const int tooltip_frame_light = 2130837608;
+			public const int notification_template_icon_bg = 2130837608;
+			
+			// aapt resource value: 0x7f020069
+			public const int notification_template_icon_low_bg = 2130837609;
+			
+			// aapt resource value: 0x7f020064
+			public const int notification_tile_bg = 2130837604;
+			
+			// aapt resource value: 0x7f020065
+			public const int notify_panel_notification_icon_bg = 2130837605;
+			
+			// aapt resource value: 0x7f020066
+			public const int tooltip_frame_dark = 2130837606;
+			
+			// aapt resource value: 0x7f020067
+			public const int tooltip_frame_light = 2130837607;
 			
 			static Drawable()
 			{
